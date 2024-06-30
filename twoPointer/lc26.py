8d69c112eef01281517cb19d71242a6a4bc86bdd
alexvbogachev <109005742+alexvbogachev@users.noreply.github.com>
2024-06-30 18:51:00 -0400

    Create lc26.py

---
+++ twoPointer/lc26.py
@@ -0,0 +1,20 @@
+# O(n) time complexity with two pointers.
+class Solution(object):
+    def removeDuplicates(self, nums):
+        l = 1
+        for r in range(1, len(nums)):
+            if nums[r] != nums[r-1]:
+                nums[l] = nums[r]
+                l += 1
+        return l
+
+# Solution that runs faster in leetcode
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # Remove duplicates and sort in place
+        nums[:] = sorted(set(nums))
+        return len(nums)
