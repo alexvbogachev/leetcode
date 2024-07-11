b8a60e64c2adc5e513f2269162c6d083cd59ae6d
alexvbogachev <109005742+alexvbogachev@users.noreply.github.com>
2024-07-11 13:09:38 -0400

    Create lc2215.py

---
+++ arrays&hashing/lc2215.py
@@ -0,0 +1,11 @@
+# O(n) time complexity
+class Solution(object):
+    def findDifference(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[List[int]]
+        """
+        set1 = set(nums1)
+        set2 = set(nums2)
+        return [set1 - set2, set2 - set1]
